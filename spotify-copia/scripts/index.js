e7eb65addf916eef1b6c4990e4a04425b6dca676
victor-raphael17 <victorraphaellima2005@gmail.com>
2026-01-22 09:26:25 -0300

    add toxicity + add playlist randomizer

--- spotify-copia/scripts/index.js
+++ spotify-copia/scripts/index.js
@@ -16,7 +16,8 @@ const nutshell = {
   artist: 'The Offspring',
   file: 'the_kids_arent_alright'
 
-}, playlist = [nutshell, one_last_breath, the_kids_arent_alright];
+}, defaultPlaylist = [nutshell, one_last_breath, the_kids_arent_alright];
+let playlist = [...defaultPlaylist];
 
 // Song info elements
 const songName = document.getElementById('song-name'),
@@ -33,52 +34,72 @@ const	play = document.getElementById('play'),
   progressContainer = document.getElementById('progress-container'),
   progress = document.getElementById('progress');
 
-// Play/Pause state
+// Song state variables
 let isSongPlaying = false;
 
-// Playlist index
-let playlistIndex = 0;
+// Playlist state variables
+let playlistIndex = 0,
+  isShuffled = false;
 
 function playSong() {
+
 	play.querySelector('i').classList.remove('bi-play-circle-fill');
 	play.querySelector('i').classList.add('bi-pause-circle-fill');
 	song.play()
 	isSongPlaying = true;
+
 };
 
 function pauseSong() {
+
 	play.querySelector('i').classList.remove('bi-pause-circle-fill');
 	play.querySelector('i').classList.add('bi-play-circle-fill');
 	song.pause()
 	isSongPlaying = false;
+
 };
 
 function togglePlayPause() {
+
 	if (isSongPlaying) {
+
 		pauseSong();
+
 	} else {
+
 		playSong();
+
 	}
+
 };
 
 function refreshPlayer() {
+
   cover.src = `songs/cover/${playlist[playlistIndex].file}.png`;
   song.src = `songs/audio/${playlist[playlistIndex].file}.mp3`;
   songName.innerText = playlist[playlistIndex].name;
   artist.innerText = playlist[playlistIndex].artist;
+
 };
 
 function playPreviousSong() {
+
   if (playlistIndex === 0) {
     playlistIndex = playlist.length - 1;
+
   } else {
+
     playlistIndex -= 1;
+
   }
+
   refreshPlayer();
   playSong();
+
 };
 
 function playNextSong() {
+
   if (playlistIndex === (playlist.length - 1)) {
     playlistIndex = 0;
   } else {
@@ -86,22 +107,67 @@ function playNextSong() {
   }
   refreshPlayer();
   playSong();
+
 };
 
 function updateProgressBar() {
+
   const barWidth = (song.currentTime / song.duration) * 100;
   progress.style.setProperty('--progress', `${barWidth}%`);
+
 }
 
 function jumpToTime(event) {
+
   const width = progressContainer.clientWidth,
     clickX = event.offsetX,
     newTime = (clickX / width) * song.duration;
     song.currentTime = newTime;
+
   }
+
+function shuffleArray() {
+
+  let index = 0,
+    auxPlaylist = [...defaultPlaylist];
+
+  console.log("before loop", playlist, defaultPlaylist, auxPlaylist);
+
+  playlist[0] = playlist[playlistIndex];
+
+  auxPlaylist.splice(playlistIndex, 1);
+  
+  index += 1;
+
+  while (index < defaultPlaylist.length) {
+
+    let randomIndex = Math.floor(Math.random() * auxPlaylist.length);
+
+    playlist[index] = auxPlaylist[randomIndex]
+    index += 1;
+    auxPlaylist.splice(randomIndex, 1);
+
+  }
+
+  console.log("playing playlist", playlist);
+
+}
+
+function toggleShuffle() {
+  if (!isShuffled) {
+    isShuffled = true;
+    shuffleArray();
+    shuffle.classList.add('active');
+  } else {
+    isShuffled = false;
+    playlist = [...defaultPlaylist];
+    shuffle.classList.remove('active');
+  }
+}
 
 // Automatically runned
 refreshPlayer();
+
 play.addEventListener('click', togglePlayPause);
 song.addEventListener('timeupdate', updateProgressBar);
 song.addEventListener('ended', playNextSong);
@@ -110,3 +176,4 @@ song.addEventListener('ended', playNextSong);
 previous.addEventListener('click', playPreviousSong);
 next.addEventListener('click', playNextSong);
 progressContainer.addEventListener('click', jumpToTime);
+shuffle.addEventListener('click', toggleShuffle);
